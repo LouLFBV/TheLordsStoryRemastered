f889d7d696c538cf407b44c040a51f933a2540a5
LouLFBV <lou.lefebvre@ynov.com>
2026-05-10 00:15:55 +0200

    marchand presque terminé

--- Assets/Scripts/OtherNew/UIProduitMarchand.cs
+++ Assets/Scripts/OtherNew/UIProduitMarchand.cs
@@ -1,15 +1,39 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems; // Obligatoire pour les interfaces de pointeur
 
-public class UIProduitMarchand : MonoBehaviour
+public class UIProduitMarchand : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [Header("UI Components")]
-    public Image iconeItem;
     public TextMeshProUGUI nameItem;
-    public TextMeshProUGUI stockItemInInventory;
+    public Image iconeItem;
     public TextMeshProUGUI priceItem;
-    public Button buyButton;
-    public Button fillStockButton;
+    public TextMeshProUGUI stockItemInInventory;
     public TextMeshProUGUI priceFillStock;
+    public Button buyButton;
+
+    // Ajoute une référence au groupe qui contient "Acheter" et "Remplir"
+    [Header("Hover Elements")]
+    public GameObject actionButtonsGroup;
+
+    private void Awake()
+    {
+        // On cache les boutons au départ
+        if (actionButtonsGroup != null)
+            actionButtonsGroup.SetActive(false);
+    }
+
+    // Appelé quand la souris (ou ton curseur manette) arrive sur le slot
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (actionButtonsGroup != null)
+            actionButtonsGroup.SetActive(true);
+    }
+
+    // Appelé quand le curseur sort du slot
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (actionButtonsGroup != null)
+            actionButtonsGroup.SetActive(false);
+    }
 }
