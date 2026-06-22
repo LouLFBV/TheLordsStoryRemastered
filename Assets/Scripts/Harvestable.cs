5b92e9b94f4592d45766ea15b1f7b4cfcded3b6b
LouLFBV <louloulefebvre33@gmail.com>
2026-06-22 23:48:51 +0200

    Harvestable save corrigée

--- Assets/Scripts/Harvestable.cs
+++ Assets/Scripts/Harvestable.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Harvestable : WorldDisappearOnCollected
+public class Harvestable : /*WorldDisappearOnCollected*/ MonoBehaviour
 {
     public Ressource[] harvestableItems;
 
